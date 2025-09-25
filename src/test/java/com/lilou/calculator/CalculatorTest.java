b166e30850f28dc990696c500e2fe78a8c768f35
Lilou DUFAU <ldufau007iutbayonne.univ-pau.fr>
2025-09-25 11:23:30 +0200

    feat : Etape 6

--- src/test/java/com/lilou/calculator/CalculatorTest.java
+++ src/test/java/com/lilou/calculator/CalculatorTest.java
@@ -1,7 +1,121 @@
 package com.lilou.calculator;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class CalculatorTest {
+    @Test
+    void add_devrait_calculer_la_somme_de_deux_int() {
+        // GIVEN
+        Calculator calc = new Calculator();
+
+        // WHEN
+        int resultat = calc.add(2,3);
+
+        // THEN
+        assertEquals(5, resultat, "2 + 3 devrait etre egal a 5");
+    }
+
+    @Test
+    void add_devrait_retourner_une_erreur_si_opG_est_var(){
+        // GIVEN
+
+        // WHEN
+
+        // THEN
+
+    }
+
+    @Test
+    void add_devrait_retourner_une_erreur_si_opD_est_var(){
+        // GIVEN
+
+        // WHEN
+
+        // THEN
+
+    }
+
+    @Test
+    void add_devrait_retourner_une_erreur_si_opG_null(){
+        // GIVEN
+
+        // WHEN
+
+        // THEN
+
+    }
+
+    @Test
+    void add_devrait_retourner_une_erreur_si_opD_null(){
+        // GIVEN
+
+        // WHEN
+
+        // THEN
+
+    }
+
+    @Test
+    void divide_devrait_calculer_la_division_de_deux_int(){
+        // GIVEN
+        Calculator calc = new Calculator();
+
+        // WHEN
+        int resultat = calc.divide(10,5);
+
+        // THEN
+        assertEquals(2,resultat, "10 divise par 5 doit donner 2");
+    }
+
+    @Test
+    void divide_devrait_retourner_une_erreur_si_opD_est_zero(){
+        // GIVEN
+
+        // WHEN
+
+        // THEN
+    }
+
+    @Test
+    void divide_devrait_retourner_une_erreur_si_opG_est_var(){
+        // GIVEN
+
+        // WHEN
+
+        // THEN
+
+    }
+
+    @Test
+    void divide_devrait_retourner_une_erreur_si_opD_est_var(){
+        // GIVEN
+
+        // WHEN
+
+        // THEN
+
+    }
+
+    @Test
+    void divide_devrait_retourner_une_erreur_si_opG_null(){
+        // GIVEN
+
+        // WHEN
+
+        // THEN
+
+    }
+
+    @Test
+    void divide_devrait_retourner_une_erreur_si_opD_null(){
+        // GIVEN
+
+        // WHEN
+
+        // THEN
+
+    }
 
 }
